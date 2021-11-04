d2b8de363c6318777fe69faca792c2df35c86b39
BearkillerPT <gilteixeira@ua.pt>
2021-11-04 13:44:21 +0000

    parser with example

---
+++ IR1/parser.py
@@ -0,0 +1,47 @@
+import sys
+
+
+class Parser:
+
+    def parseDataFile(self):
+        self.headerData = []
+        header = self.dataFile.readline().split('\t')
+        for header_id in header:
+            self.headerData.append(header_id.replace('\n', ''))
+        self.documents = {}
+        while data := self.dataFile.readline().split('\t'):
+            if not data or data==['']:
+                break
+            if len(data) != len(self.headerData):
+                continue
+            doc_id_i = self.headerData.index('customer_id')
+            prod_title_i = self.headerData.index('product_title')
+            review_headline_i = self.headerData.index('review_headline')
+            review_body_i = self.headerData.index('review_body')
+            review = {
+                    'product_title': data[prod_title_i],
+                    'review_headline': data[review_headline_i],
+                    'review_body': data[review_body_i]
+                }
+            if data[doc_id_i] not in self.documents.keys():
+                self.documents.setdefault(data[doc_id_i], [review])
+            else:
+                self.documents[data[doc_id_i]].append(review)
+
+    def __init__(self, dataFileName=None):
+        if not dataFileName:
+            dataFileName = 'amazon_reviews_us_Camera_v1_00.tsv'
+        try:
+            self.dataFile  = open(dataFileName, encoding="utf8")
+        except Exception as e:
+            self.dataFile = None
+            print(e)
+        if self.dataFile:
+            self.parseDataFile()
+
+if __name__=='__main__':
+    if len(sys.argv) == 1:
+        a = Parser()
+    elif len(sys.argv) == 2:
+        a = Parser(dataFileName=sys.argv[1])
+    
---
+++ IR1/readme.md
@@ -0,0 +1,3 @@
+```
+This repository contains Infromation Retrieval assignments.
+```
